de2a34f1ccd1fcc0c4e87cc02721bbd4e2c3a201
Rayan Selmi <72416925+rs-4@users.noreply.github.com>
2023-10-12 21:04:54 +0200

    feat:homepage

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from "react";
+import NavBar from "../components/navbar";
 
 function index() {
 
   const [data, setData] = useState<string>("Loading...");
 
-  useEffect(() => {
-    fetch("http://localhost:8080/")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setData(data.message);
-      });
-  }, []);
 
-  return <div>{data}</div>;
+  return <div></div>;
 }
 
 export default index;
---
+++ client/pages/plan/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function plan() {
+  return (
+    <div>plan</div>
+  )
+}
